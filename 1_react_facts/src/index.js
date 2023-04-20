ce0ee876e0ce544bc5badddcc4864542958603dd
bliv27 <david.raffin01@gmail.com>
2023-04-20 10:20:36 +1000

    info 16

--- 1_react_facts/src/index.js
+++ 1_react_facts/src/index.js
@@ -1,26 +1,28 @@
-// 1. Why do we need to `import React from "react"` in our files?
+/**
+Challenge: 
 
-// jsx syntax is required 
+Part 1: Create a page of your own using a custom Page component
 
-// 2. If I were to console.log(page) in index.js, what would show up?
+It should return an ordered list with the reasons why you're
+excited to be learning React :)
 
-// an object that contains a list of all the properties of the jsx
+Render your list to the page
 
-// 3. What's wrong with this code:
-// ```
-// const page = (
-//     <h1>Hello</h1>
-//     <p>This is my website!</p>
-// )
-// ```
+ */
 
-// there can only be 1 element in a jsx object with as many children as they need
+import ReactDOM from 'react-dom/client'
 
-// 4. What does it mean for something to be "declarative" instead of "imperative"?
+function Page(){
+  return (
+    <div>
+      <ul>
+        <li>one </li>
+        <li>two</li>
+        <li>three</li>
+      </ul>
+    </div>
+  )
+}
 
-// declarative tells the computer how to render and it figures it out, 
-// imperative means you need to explain every step of a process
-
-// 5. What does it mean for something to be "composable"?
-
-// create a larger thing out of many smaller components
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Page />)
