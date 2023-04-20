f74a7a0818888dcb89c6b500a3af5e4992e1d448
bliv27 <david.raffin01@gmail.com>
2023-04-20 11:00:17 +1000

    info 21

--- 1_react_facts/src/index.js
+++ 1_react_facts/src/index.js
@@ -1,53 +1,13 @@
 import ReactDOM from "react-dom/client"
 import './index.css';
+import Header from './Header';
+import Footer from './Footer';
+import MainContent from "./MainContent";
 
 /**
-Challenge: 
-
-- Add an `ul` inside the Header's `nav` and create
-  the following `li`s: "Pricing", "About", & "Contact"
-- Using flexbox, line up the nav items horizontally, and
-  put them inline with the React logo.
-- Change the image styling to happen in CSS instead of in-line
-  For practice, add a new class to the image in order to style it
+Challenge: move the Footer and MainContent components
+into their own files.
 */
-
-function Header() {
-    return (
-        <header>
-            <nav>
-                <img src="./react-logo.png" />
-                <ul className="nav-items">
-                  <li>Pricing</li>
-                  <li>About</li>
-                  <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
-
-function Footer() {
-    return (
-        <footer>
-            <small>© 2021 Ziroll development. All rights reserved.</small>
-        </footer>
-    )
-}
-
-function MainContent() {
-    return (
-        <div>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>It's a popular library, so I'll be 
-                able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer
-                if I know React</li>
-            </ol>
-        </div>
-    )
-}
 
 function Page() {
     return (
