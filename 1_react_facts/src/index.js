2078e88e905966e2237eefe0980a591b5abb55b1
bliv27 <david.raffin01@gmail.com>
2023-04-20 10:45:07 +1000

    info 20

--- 1_react_facts/src/index.js
+++ 1_react_facts/src/index.js
@@ -1,63 +1,63 @@
-
 import ReactDOM from "react-dom/client"
-
-/**
-Mini Challenge:
-Move the `header` element from Page into 
-its own component called "Header"
-*/
+import './index.css';
 
 /**
 Challenge: 
 
-- Move the `footer` into its own component called "Footer" 
-  and render that component inside the Page component.
-- Move the `h1` and `ol` together into another component
-  called "MainContent" and render inside Page as well.
+- Add an `ul` inside the Header's `nav` and create
+  the following `li`s: "Pricing", "About", & "Contact"
+- Using flexbox, line up the nav items horizontally, and
+  put them inline with the React logo.
+- Change the image styling to happen in CSS instead of in-line
+  For practice, add a new class to the image in order to style it
 */
 
 function Header() {
-  return(
-    <header>
-      <nav>
-        <img src="./react-logo.png" width="40px" />
-      </nav>
-    </header>
-  )
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" />
+                <ul className="nav-items">
+                  <li>Pricing</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
 
-function Footer(){
-  return(
-    <footer>
-      <small>© 2021 Ziroll development. All rights reserved.</small>
-    </footer>
-  )
+function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
 }
 
-function MainContent(){
-  return(
-    <div>
-      <h1>Reasons I'm excited to learn React</h1>
-      <ol>
-        <li>It's a popular library, so I'll be 
-        able to fit in with the cool kids!</li>
-        <li>I'm more likely to get a job as a developer
-        if I know React</li>
-      </ol>
-    </div>
-  )
+function MainContent() {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+        </div>
+    )
 }
 
 function Page() {
-  return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
 }
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <Page />
-)
+root.render(<Page/>)
