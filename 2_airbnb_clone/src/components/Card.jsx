49e0e16b243b21717ad2503100c15b401e52f4a1
bliv27 <david.raffin01@gmail.com>
2023-04-26 13:16:02 +1000

    airbnb 15

--- 2_airbnb_clone/src/components/Card.jsx
+++ 2_airbnb_clone/src/components/Card.jsx
@@ -1,19 +1,21 @@
-import katie from "./../assets/katie-Zaferes.png";
 import star from "./../assets/star.png";
 
-export default function Card() {
+export default function Card(props) {
   return(
     <div className="card">
       <div className="card__status">sold out</div>
-      <img className="card__photo" src={katie} />
+      <img 
+        className="card__photo" 
+        src={new URL("./../assets/"+props.img, import.meta.url).href } 
+      />
       <p className="card__stats">
         <img className="card__star" src={star} />
-        <span>5.0</span>
-        <span className="grey">(6) &#x2022; </span>
-        <span className="grey">USA</span>
+        <span>{props.rating}</span>
+        <span className="grey">({props.reviewCount}) &#x2022; </span>
+        <span className="grey">{props.country}</span>
       </p>
-      <p>Life lessons with Katie Zaferes</p>
-      <p><span className="bold">From $136</span> / person</p>
+      <p>{props.title}</p>
+      <p><span className="bold">From ${props.price}</span> / person</p>
     </div>
   )
 }
