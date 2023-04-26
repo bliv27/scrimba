56d06d2184dd318aae052211bb40eacebe7a4e4e
bliv27 <david.raffin01@gmail.com>
2023-04-26 14:49:15 +1000

    airbnb 25

--- 2_airbnb_clone/src/components/Card.jsx
+++ 2_airbnb_clone/src/components/Card.jsx
@@ -3,9 +3,9 @@ import star from "./../assets/star.png";
 export default function Card(props) {
 
   let badeText
-  if (props.item.openSpots === 0){
+  if (props.openSpots === 0){
     badeText = "sold out"
-  } else if (props.item.location === "Online") {
+  } else if (props.location === "Online") {
     badeText = "online"
   }
 
@@ -14,16 +14,16 @@ export default function Card(props) {
       {badeText && <div className="card__status">{badeText}</div>}
       <img 
         className="card__photo" 
-        src={new URL("./../assets/"+props.item.coverImg, import.meta.url).href } 
+        src={new URL("./../assets/"+props.coverImg, import.meta.url).href } 
       />
       <p className="card__stats">
         <img className="card__star" src={star} />
-        <span>{props.item.stats.rating}</span>
-        <span className="grey">({props.item.stats.reviewCount}) &#x2022; </span>
-        <span className="grey">{props.item.location}</span>
+        <span>{props.stats.rating}</span>
+        <span className="grey">({props.stats.reviewCount}) &#x2022; </span>
+        <span className="grey">{props.location}</span>
       </p>
-      <p className="card__title">{props.item.title}</p>
-      <p className="card__price"><span className="bold">From ${props.item.price}</span> / person</p>
+      <p className="card__title">{props.title}</p>
+      <p className="card__price"><span className="bold">From ${props.price}</span> / person</p>
     </div>
   )
 }
